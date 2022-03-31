aed3c7ccb5cb84be087c206cf07b2b262de9b5c6
TheRealSumpark <sumpark@hotmail.fr>
2022-03-31 12:07:56 +0100

    added sound + colour transition + transition from bio page to projects page

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 import "./style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Howl, Howler } from "howler";
+import $ from "jquery";
+import gsap from "gsap";
+
 import {
   AdditiveBlending,
   MeshBasicMaterial,
@@ -9,7 +14,7 @@ import {
   SubtractiveBlending,
 } from "three";
 
-let renderer, camera, scene, torus, particlesMesh;
+let renderer, camera, scene, torus, particlesMesh, controls, state;
 
 const canvasContainer = document.querySelector("#canvasContainer");
 function init() {
@@ -36,6 +41,8 @@ function init() {
     posArray[i] = (Math.random() - 0.5) * 13;
   }
 
+  console.log(posArray);
+
   particlesGeometry.setAttribute(
     "position",
     new THREE.BufferAttribute(posArray, 3)
@@ -55,6 +62,7 @@ function init() {
   const pointLight = new THREE.PointLight(0xffffff, 1);
   pointLight.position.set(0, 0, 100);
 
+  //camera
   camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -70,6 +78,9 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setClearColor(new THREE.Color(0x220828));
+
+  //camera controls
+  controls = new OrbitControls(camera, renderer.domElement);
 }
 
 function onWindowResize() {
@@ -100,11 +111,7 @@ function animateParticles(event) {
  * Animate
  */
 
-const clock = new THREE.Clock();
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
   // Update objects
   torus.rotation.y += 0.002;
 
@@ -123,7 +130,156 @@ const tick = () => {
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
+
+  /**
+   * state used for navigation
+   *
+   * states :
+   * bio_page
+   * projects_page
+   * contact_page
+   *
+   */
+  state = "bio_page";
+  $("#projects_page").hide();
 };
 
 init();
 tick();
+
+var contactButton = document.getElementById("contactButton");
+contactButton.addEventListener("click", function (event) {
+  const timeline = gsap.timeline();
+
+  timeline.to(particlesMesh.position, {
+    delay: 0.5,
+    z: -990,
+    duration: 3,
+    ease: Power2.easeOut,
+  });
+
+  timeline.to($("#projects_page:before"), {
+    zIndex: 1,
+    ease: Power3.easeIn,
+    display: "block",
+  });
+
+  gsap.to([$("#bio_page"), $("#menu")], {
+    delay: 0.4,
+    opacity: 0,
+    display: "none",
+    ease: Power3.easeIn,
+    duration: 1,
+  });
+
+  gsap.to(torus.position, {
+    z: 10,
+    duration: 1,
+    ease: Power2.easeIn,
+  });
+
+  gsap.to(camera.position, {
+    delay: 0.5,
+    z: -990,
+    duration: 3,
+    ease: Power2.easeOut,
+  });
+
+  //animate color when transitioning
+  var color = renderer.getClearColor();
+  gsap.to(color, {
+    r: 0,
+    g: 0,
+    b: 0,
+    duration: 2,
+    ease: Power2.easeIn,
+    onUpdate() {
+      renderer.setClearColor(color);
+    },
+  });
+
+  controls.update();
+
+  console.log(particlesMesh.position);
+});
+
+/**
+ * Sound
+ */
+
+// set up the site default sounds
+
+let soundOn = true;
+var sound = new Howl({
+  src: ["sounds/arpeggios_from_heaven.mp3"],
+  loop: true,
+  volume: 0.5,
+  html5: true,
+
+  name: "arpeggios_from_heaven",
+});
+
+sound.play(0);
+
+//global mute control
+const muteAll = function (ignoreGlobalSoundState) {
+  console.log("mute all ");
+  $(".footer-sound .sbar").addClass("noAnim");
+  clearInterval(window.muteInterval);
+  var v = Howler.volume();
+  window.muteInterval = setInterval(function () {
+    v -= 0.1;
+    Howler.volume(v);
+    if (v <= 0.0) {
+      Howler.volume(0.0);
+      clearInterval(window.muteInterval);
+    }
+    //console.log("ticking");
+  }, 50);
+
+  //_this.analyser.minDecibels = -100;
+
+  // flag will
+  if (!ignoreGlobalSoundState) {
+    soundOn = false;
+  }
+};
+
+// global unmute control
+const unMuteAll = function (ignoreGlobalSoundState) {
+  console.log("unMute all ");
+  $(".footer-sound .sbar").removeClass("noAnim");
+  clearInterval(window.muteInterval);
+  var v = Howler.volume();
+  window.muteInterval = setInterval(function () {
+    v += 0.1;
+    Howler.volume(v);
+    if (v >= 1) {
+      Howler.volume(1);
+      clearInterval(window.muteInterval);
+    }
+    //console.log("ticking");
+  }, 50);
+
+  //_this.analyser.minDecibels = -60;
+
+  // flag will
+  if (!ignoreGlobalSoundState) {
+    soundOn = true;
+  }
+};
+
+$(".footer-sound").click(function (e) {
+  sound.play("arpeggios_from_heaven");
+
+  //_this.audio.playFromTo("click",0,1);
+  if (soundOn) {
+    $(".footer-sound .sbar").addClass("noAnim");
+    soundOn = false;
+    muteAll();
+  } else {
+    $(".footer-sound .sbar").removeClass("noAnim");
+    soundOn = true;
+    unMuteAll();
+  }
+});
